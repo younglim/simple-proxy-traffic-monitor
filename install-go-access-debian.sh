526ddb93ade87eed73aa5f0f33aefdfc0b295081
younglim <younglim@users.noreply.github.com>
2019-04-04 18:03:24 +0800

    Install goaccess

---
+++ install-go-access-debian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "deb https://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list
+wget -O - https://deb.goaccess.io/gnugpg.key | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install goaccess -y
