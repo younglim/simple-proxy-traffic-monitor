da223ae52f9f603dc5ea319547923d5895765a1b
YangKaiting <42689576+YangKaiting@users.noreply.github.com>
2019-04-08 18:18:08 +0800

    Update start-squid-live-report.sh
    Amended to include a css file link to the original report and output to report2.html

--- start-squid-live-report.sh
+++ start-squid-live-report.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/bin/goaccess /var/log/squid/access.log --log-format='%x.%^ %~%L %h %^/%s %b %m %U %e %^' --time-format='%s' --date-format='%s' -o /var/www/html/report.html --real-time-html
+/usr/bin/goaccess /var/log/squid/access.log --log-format='%x.%^ %~%L %h %^/%s %b %m %U %e %^'  --time-format='%s' --date-format='%s' -o /var/www/html/report.html --real-time-html &
+goaccess=$!
+echo $goaccess
+sed 's/<\/body>/&<link rel="stylesheet" type="text\/css" href="extra-style.css" \/> /' /var/www/html/report.html  > /var/www/html/report2.html
+wait
