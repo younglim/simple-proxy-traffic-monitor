9b98dfc3714bdc2729b5cbcb4b6ab43fbd553535
younglim <younglim@users.noreply.github.com>
2019-04-08 18:26:06 +0800

    Create report2.html

--- install-nginx.sh
+++ install-nginx.sh
@@ -3,3 +3,5 @@
 sudo apt install nginx -y
 sudo touch /var/www/html/report.html
 sudo chown proxy:proxy /var/www/html/report.html
+sudo touch /var/www/html/report2.html
+sudo chown proxy:proxy /var/www/html/report2.html
